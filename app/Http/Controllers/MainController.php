b2d24bab4e307e3dd3bf9d50a40bd8d369d773a7
Jhonatham Jesus <jhonathampro@gmail.com>
2025-08-06 17:15:24 -0300

    feat: add new mote view

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -10,20 +10,21 @@ class MainController extends Controller
    public  function index(){
       // load user's notes
       $id = session('user.id');
-      $user = User::find($id)->toArray();
       $notes = User::find($id)->notes()->get()->toArray();
 
-      echo '<pre>';
-      print_r($user);
-      print_r($notes);
 
-      die();
        // show home view
-       return view('home');
+       return view('home', ['notes' => $notes]);
    }
 
    public function newNote()
    {
        echo "I'm inside the app!";
    }
+
+   public function editNote($id)
+   {
+
+   }
+
 }
