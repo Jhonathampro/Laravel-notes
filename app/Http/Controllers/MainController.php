3c67e6c15dac72b4eb52bb570baf58d461c48ae9
Jhonatham Jesus <jhonathampro@gmail.com>
2025-08-15 17:36:39 -0300

    feat: add function to edit notes

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -63,7 +63,50 @@ class MainController extends Controller
    public function editNote($id)
    {
        $id = Oprerations::decryptId($id);
-       echo "I'm editing note witch id = $id";
+
+       // load note
+       $note = Note::find($id);
+
+       // show edit note view
+       return view('edit_note', ['note' => $note]);
+   }
+
+   public function editNoteSubmit(Request $request){
+       // validate request
+       $request->validate([
+           // rules
+           'text_title'=>'required|min:3|max:200',
+           'text_note'=>'required|min:3|max:3000'
+       ],
+           [
+               // error messages
+               'text_title.required'=>'O titulo é obrigatorio',
+               'text_title.max'=>'O titulo deve ter no máximo :max caracteres',
+               'text_title.min'=>'O titulo deve ter pelo menos :min caracteres',
+               'text_note.required'=>'A nota deve ser obrigatoria',
+               'text_note.min'=>'A nota deve ter pelo menos :min caracteres',
+               'text_note.max'=>'A nota deve ter no máximo :max caracteres',
+           ]
+       );
+
+       // check if note_id exists
+       if($request->note_id == null){
+           return redirect()->route('home');
+       }
+       // dercrypt note_id
+       $id = Oprerations::decryptId($request->note_id);
+
+       // load note
+       $note = Note::find($id);
+
+       // update note
+       $note->title = $request->text_title;
+       $note->text = $request->text_note;
+       $note->save();
+
+       // redirect to home
+      return redirect()->route('home');
+
    }
 
     public function DeleteNote($id)
